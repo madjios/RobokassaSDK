1a172478cbc3801f4fb0be38817b9c0b8ada487f
madjios <majit.ios.dev@icloud.com>
2024-09-26 17:18:34 +0500

    adding SPM

---
+++ RobokassaSDK/Sources/RobokassaSDK/RobokassaSDK.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct Robokassa {
+    public static func start(login: String, password: String) {
+        print("LOGIN: \(login)\nPASSWORD: \(password)")
+    }
+}
